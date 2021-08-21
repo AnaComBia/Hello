09a0b83232dc728502b9312448cc59b2ce4d3fa3
AnaComBia <anabeatrizetorres@gmail.com>
2021-08-20 23:28:58 -0300

    Finaliza o programa Hello

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Olá usuário\nMe chamo Ana Beatriz!");
+
+
+            Console.ResetColor();
+        }
+    }
+}
